dfa0e89fa6d8f19359a09ea544ea47a70910b064
glenn <glenn@glenn-Lenovo-G480>
2015-09-09 23:47:24 +0800

    added new file

---
+++ ilo/mou/vocabulary/country.py
@@ -0,0 +1,26 @@
+from five import grok
+from zope.schema.interfaces import IContextSourceBinder
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+from zope.schema.interfaces import IVocabularyFactory
+from zope.component import getUtility
+
+from p01.vocabulary.country import ISO3166Alpha2CountryVocabulary
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+import copy
+
+class Country(grok.GlobalUtility):
+    grok.name('ilo.mou.country')
+    grok.implements(IVocabularyFactory)
+
+    def __call__(self, context):
+        results = []
+        data = []
+        vocabs = ISO3166Alpha2CountryVocabulary(context)
+        for vocab in vocabs:
+            results.append({'title':vocab.title, 'token':vocab.token, 'value':vocab.value})
+        if results:
+            results.sort(key=lambda k: k['title'])
+        for result in results:
+            data.append(SimpleTerm(value=result['value'], token=result['token'], title=result['title']))
+        return SimpleVocabulary(data)
+            
